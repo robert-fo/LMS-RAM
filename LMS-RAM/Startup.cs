eeca29630784816496d5c9c727d03817ef3a4abd
robert-fo <robert.fo@gmail.com>
2016-03-16 15:35:08 +0100

    Initial

---
+++ LMS-RAM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMS_RAM.Startup))]
+namespace LMS_RAM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
