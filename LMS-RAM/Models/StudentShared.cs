d6756b744bbd7ef712a8f942e37da9b4677bab0e
annhuh <huhta@hotmail.se>
2016-03-17 10:20:33 +0100

    Initial StudentShared model

---
+++ LMS-RAM/Models/StudentShared.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_RAM.Models
+{
+    public class StudentShared
+    {
+        [Display(Name = "Beskrivning")] 
+        public String Description { get; set; }
+        [Display(Name = "Filnamn")] 
+        public String FileName { get; set; }
+
+        [ForeignKey("CourseId")]
+        public virtual Course Course { get; set; }
+        [ForeignKey("StudentId")]
+        public virtual Student Student { get; set; }
+
+    }
+}
