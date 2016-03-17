d1c328ae6216cb0392857a85fde8624873ec1219
annhuh <huhta@hotmail.se>
2016-03-17 09:43:08 +0100

    Initial Teacher model

---
+++ LMS-RAM/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_RAM.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, Display(Name="Personnr")]
+        public String SSN { get; set; }
+        [Required, Display(Name = "Förnamn")]
+        public String FirstName { get; set; }
+        [Required, Display(Name = "Efternamn")]
+        public String LastName { get; set; }
+
+    }
+}
