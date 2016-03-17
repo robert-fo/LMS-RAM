633fc1b118baa1e688a4c7a5fe47e9a7c5609785
annhuh <huhta@hotmail.se>
2016-03-17 13:03:11 +0100

    New Grade (enum)

---
+++ LMS-RAM/Models/Grade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_RAM.Models
+{
+    public enum Grade
+    {
+        VG = 1, G = 2, U = 3
+    }
+
+}
