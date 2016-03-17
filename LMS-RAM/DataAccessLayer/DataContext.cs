d2553dd2569c7e9e933d8e22e0691476554c8b6c
annhuh <huhta@hotmail.se>
2016-03-17 14:13:48 +0100

    DataAccessLayer added

---
+++ LMS-RAM/DataAccessLayer/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LMS_RAM.DataAccessLayer
+{
+    public class DataContext : DbContext
+    {
+    }
+}
