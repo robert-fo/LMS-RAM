a4047fb04a10a81c4ac981c067e2f1bc6f997ea7
annhuh <huhta@hotmail.se>
2016-03-23 09:42:35 +0100

    commit of Migrations, LMS_RAM.csproj and Web.config

--- LMS-RAM/Migrations/201603221438510_Initial.Designer.cs
+++ LMS-RAM/Migrations/201603221438510_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace LMS_RAM.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Initial1 : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201603221037216_Initial1"; }
+            get { return "201603221438510_Initial"; }
         }
         
         string IMigrationMetadata.Source
