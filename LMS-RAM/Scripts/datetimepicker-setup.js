9afb07a6d154f9421b411ff45d0fb6ea178e3694
robert-fo <robert.fo@gmail.com>
2016-04-05 12:18:39 +0200

    DateTimePicker started

---
+++ LMS-RAM/Scripts/datetimepicker-setup.js
@@ -0,0 +1,40 @@
+﻿//$(function () {
+
+//    $('.datetimepicker').datetimepicker({
+//        pickDate: true,                 //en/disables the date picker
+//        pickTime: true,                 //en/disables the time picker
+//        useMinutes: true,              //en/disables the minutes picker
+//        useSeconds: false,              //en/disables the seconds picker
+//    });
+
+//    $('#AnnualMeetingDate').datetimepicker({
+
+//        defaultDate: '@Model.AnnualMeetingDate',
+
+//        format: 'L',
+
+//        showClose: true,
+
+//        showClear: true,
+
+//        toolbarPlacement: 'top'
+
+//    });
+
+//    $('#AnnualMeetingStartTime').datetimepicker({
+
+//        defaultDate: '@Model.AnnualMeetingStartTime',
+
+//        format: 'LT',
+
+//        showClose: true,
+
+//        showClear: true,
+
+//        toolbarPlacement: 'top',
+
+//        stepping: 15
+
+//    });
+
+//});
